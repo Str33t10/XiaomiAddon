dd10dbf63edc4edfb7daebdcdcf8f47884cb19d1
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-08-11 08:56:23 +0300

    Update action.sh

--- action.sh
+++ action.sh
@@ -4,5 +4,5 @@ am start -n "com.android.camera/.CameraPreferenceActivity" >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     exit 0
 else
-    echo "! Xiaomi Camera not found"
+    echo "! Camera not found"
 fi
