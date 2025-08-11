407d442c9821f8f0da1b966051cff3cd36a2569b
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-08-11 06:17:45 +0300

    Add files via upload

---
+++ webui/index.js
@@ -0,0 +1,44 @@
+/**
+ * Execute shell commands with ksu.exec
+ * @param {string} command -  shell command to execute
+ * @returns {Promise<string>} - A promise that resolves with stdout content
+ * @throws {Error} If command execution fails with:
+ *   - stderr in error message
+ */
+async function exec(command) {
+    const callbackName = `exec_callback_${Date.now()}`;
+    return new Promise((resolve, reject) => {
+        window[callbackName] = (errno, stdout, stderr) => {
+            delete window[callbackName];
+            errno === 0 ? resolve(stdout) : reject(stderr);
+        };
+        try {
+            ksu.exec(command, "{}", callbackName);
+        } catch (error) {
+            console.error("Unable to execute command:", error)
+        }
+    });
+}
+
+// Function to toast message
+function toast(message) {
+    try {
+        ksu.toast(message);
+    } catch (error) {
+        console.error("Unable to toast message:", error)
+    }
+}
+
+// Initial load
+document.addEventListener('DOMContentLoaded', async () => {
+     exec('am start -n com.android.camera/.CameraPreferenceActivity')
+    .then(() => {
+      toast('Xiaomi Camera Preference')
+      console.log('Camera is ready!')
+      })
+    .catch(err => {
+      toast('Xiaomi Camera not found')
+      console.log('Camera is not ready!')
+      console.error(err)
+    })
+});
