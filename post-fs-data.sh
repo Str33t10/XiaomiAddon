b80bc63ebef69c4dd15d3217227ec0e5a37f5300
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-22 17:10:09 +0200

    Refactor post-fs-data.sh to eliminate duplicate check
    Removed redundant wait loop for module directory.

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,5 +1,9 @@
 MODPATH=${0%/*}
 
+while [ -z "$(ls -A /data/adb/modules/)" ]; do
+    sleep 1
+done
+
 CAM_MODPATH="/data/adb/modules/xiaomicamera"
 if [ ! -d "$CAM_MODPATH" ] || [ -f "$CAM_MODPATH/remove" ]; then
     if [ -f "$MODPATH/action.sh" ]; then
@@ -11,10 +15,6 @@ if [ ! -d "$CAM_MODPATH" ] || [ -f "$CAM_MODPATH/remove" ]; then
         touch "$MODPATH/remove"
     fi
 fi
-
-while [ -z "$(ls -A /data/adb/modules/)" ]; do
-    sleep 1
-done
 
 [ -L "$CAM_MODPATH/action.sh" ] && rm -f "$CAM_MODPATH/action.sh"
 [ -L "$CAM_MODPATH/webroot" ] && rm -f "$CAM_MODPATH/webroot"
