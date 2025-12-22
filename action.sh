cb53e7746e08bf3bc6aa9e3dcb001af4354121f1
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-22 07:02:51 +0200

    Fix command syntax in action.sh

--- action.sh
+++ action.sh
@@ -2,7 +2,7 @@ MODPATH=${0%/*}
 
 if [ -z "$TMPDIR" ] && [ ! -z "$MAGISKTMP" ]; then
    pm path com.android.camera >/dev/null 2>&1 && {
-      am start -n "com.android.camera/.CameraPreferenceActivity" -e id "xiaomicamera"
+      am start -n "com.android.camera/.CameraPreferenceActivity"
       exit 0
    }
 fi
