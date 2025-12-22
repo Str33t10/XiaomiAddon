415e8c806ac8c30e35ddbacc6cfb1152599d4e73
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-22 09:00:37 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,10 @@
-MODPATH=${0%/*}
 CAM_MODPATH="/data/adb/modules/xiaomicamera"
 
 # Remove residue.
 rm -rf "/data/adb/modules/.xiaomiaddon"
 if [ -d "$CAM_MODPATH" ]; then
-    [ -L "$CAM_MODPATH/webroot" ] && rm -f "$CAM_MODPATH/webroot"
     [ -L "$CAM_MODPATH/action.sh" ] && rm -f "$CAM_MODPATH/action.sh"
+    [ -L "$CAM_MODPATH/webroot" ] && rm -f "$CAM_MODPATH/webroot"
 fi
 
-#EOF
+# EOF
