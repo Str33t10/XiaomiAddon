f26047461a5845ef66bc30ce0c5dd698a88dd97d
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-22 07:29:52 +0200

    Add symlinks for action.sh and web-ui
    Added symlinks for action.sh and web-ui to the camera module path. Removed redundant symlink checks.

--- service.sh
+++ service.sh
@@ -1,10 +1,21 @@
 MODPATH=${0%/*}
 
 CAM_MODPATH="/data/adb/modules/xiaomicamera"
-ADD_MODPATH="/data/adb/modules/.xiaomiaddon"
+
+# Symlink action.sh
+if [ -f "$MODPATH/action.sh" ] && [ ! -e "$CAM_MODPATH/action.sh" ]; then
+    ln -s "$MODPATH/action.sh" "$CAM_MODPATH/action.sh"
+fi
+
+# Symlink web-ui
+if [ ! -e "$CAM_MODPATH/webroot" ]; then
+    ln -s "$MODPATH/webui" "$CAM_MODPATH/webroot"
+fi
+
+PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 
 # Hide module from magisk manager
-PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
+ADD_MODPATH="/data/adb/modules/.xiaomiaddon"
 if [ -f "$MODPATH/action.sh" ]; then
     if [ "$MODPATH" != "$ADD_MODPATH" ]; then
         rm -rf "$ADD_MODPATH"
@@ -19,14 +30,6 @@ fi
 
 # Hide module from other manager
 rm -f "$MODPATH/module.prop"
-
-# Symlink leica preference
-if [ -f "$MODPATH/action.sh" ] && [ ! -e "$CAM_MODPATH/action.sh" ]; then
-    ln -s "$MODPATH/action.sh" "$CAM_MODPATH/action.sh"
-fi
-if [ ! -e "$CAM_MODPATH/webroot" ]; then
-    ln -s "$MODPATH/webui" "$CAM_MODPATH/webroot"
-fi
 
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 1
