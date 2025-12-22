040a1fbe85b2f768e53c741b25bcccb9b9f364ce
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-22 07:33:56 +0200

    Refactor module check loop in post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,10 +1,6 @@
 MODPATH=${0%/*}
 CAM_MODPATH="/data/adb/modules/xiaomicamera"
 
-while [ -z "$(ls -A /data/adb/modules/)" ]; do
-    sleep 1
-done
-
 if [ ! -d "$CAM_MODPATH" ] || [ -f "$CAM_MODPATH/remove" ]; then
     if [ -f "$MODPATH/action.sh" ]; then
         [ -d "/data/adb/modules/xiaomiaddon" ] && rm -rf "/data/adb/modules/xiaomiaddon"
@@ -15,6 +11,10 @@ if [ ! -d "$CAM_MODPATH" ] || [ -f "$CAM_MODPATH/remove" ]; then
         touch "$MODPATH/remove"
     fi
 fi
+
+while [ -z "$(ls -A /data/adb/modules/)" ]; do
+    sleep 1
+done
 
 [ -L "$CAM_MODPATH/webroot" ] && rm -f "$CAM_MODPATH/webroot"
 [ -L "$CAM_MODPATH/action.sh" ] && rm -f "$CAM_MODPATH/action.sh"
