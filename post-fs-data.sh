9e5b3ae48d28f01dbd53bd029072620333d6e1de
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-23 08:22:47 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,21 +1,20 @@
 MODPATH=${0%/*}
+CAM_MODPATH="/data/adb/modules/xiaomicamera"
 
 while [ -z "$(ls -A /data/adb/modules/)" ]; do
     sleep 1
 done
 
-if [ ! -d "/data/adb/modules/xiaomicamera" ] || [ -f "/data/adb/modules/xiaomicamera/remove" ]; then
+if [ ! -d "$CAM_MODPATH" ] || [ -f "$CAM_MODPATH/remove" ]; then
     if [ -f "$MODPATH/action.sh" ]; then
         [ -d "/data/adb/modules/xiaomiaddon" ] && rm -rf "/data/adb/modules/xiaomiaddon"
         cp -rf "$MODPATH" "/data/adb/modules/xiaomiaddon"
         touch "/data/adb/modules/xiaomiaddon/remove"
     else
-        mv -f "$MODPATH/module.prop.bak" "$MODPATH/module.prop"
+        mv -f "$MODPATH/config.sh" "$MODPATH/module.prop"
         touch "$MODPATH/remove"
     fi
 fi
 
-[ -L "/data/adb/modules/xiaomicamera/action.sh" ] && rm -f "/data/adb/modules/xiaomicamera/action.sh"
-[ -L "/data/adb/modules/xiaomicamera/webroot" ] && rm -f "/data/adb/modules/xiaomicamera/webroot"
-
-# EOF
+[ -L "$CAM_MODPATH/webroot" ] && rm -f "$CAM_MODPATH/webroot"
+[ -L "$CAM_MODPATH/action.sh" ] && rm -f "$CAM_MODPATH/action.sh"
