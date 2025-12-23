1ebc59b7e12f30a272134a92a260417560d72d7e
Str33t10 <37408699+Str33t10@users.noreply.github.com>
2025-12-23 08:21:56 +0200

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,34 +1,31 @@
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
-MODPATH=${0%/*}
 
-# Handle sensitive prop in background
-sh "$MODPATH/prop.sh" &
+MODPATH=${0%/*}
+CAM_MODPATH="/data/adb/modules/xiaomicamera"
+ADD_MODPATH="/data/adb/modules/.xiaomiaddon"
 
 # Hide module from magisk manager
-ADDON_MODPATH="/data/adb/modules/.xiaomiaddon"
 if [ -f "$MODPATH/action.sh" ]; then
-    if [ "$MODPATH" != "$ADDON_MODPATH" ]; then
-        rm -rf "$ADDON_MODPATH"
-        mkdir -p "$ADDON_MODPATH"
-        busybox chcon --reference="$MODPATH" "$ADDON_MODPATH"
-        cp -af "$MODPATH/." "$ADDON_MODPATH/"
+    if [ "$MODPATH" != "$ADD_MODPATH" ]; then
+        rm -rf "$ADD_MODPATH"
+        mkdir -p "$ADD_MODPATH"
+        busybox chcon --reference="$MODPATH" "$ADD_MODPATH"
+        cp -af "$MODPATH/." "$ADD_MODPATH/"
     fi
-    MODPATH="$ADDON_MODPATH"
+    MODPATH="$ADD_MODPATH"
 else
-    [ -d "$ADDON_MODPATH" ] && rm -rf "$ADDON_MODPATH"
+    [ -d "$ADD_MODPATH" ] && rm -rf "$ADD_MODPATH"
 fi
 
 # Hide module from other manager
 rm -f "$MODPATH/module.prop"
 
-# Symlink magisk action.sh
-if [ -f "$MODPATH/action.sh" ] && [ ! -e "/data/adb/modules/xiaomicamera/action.sh" ]; then
-    ln -s "$MODPATH/action.sh" "/data/adb/modules/xiaomicamera/action.sh"
+# Symlink leica preference
+if [ -f "$MODPATH/action.sh" ] && [ ! -e "$CAM_MODPATH/action.sh" ]; then
+    ln -s "$MODPATH/action.sh" "$CAM_MODPATH/action.sh"
 fi
-
-# Symlink kernelsu web-ui
-if [ ! -e "/data/adb/modules/xiaomicamera/webroot" ]; then
-    ln -s "$MODPATH/webui" "/data/adb/modules/xiaomicamera/webroot"
+if [ ! -e "$CAM_MODPATH/webroot" ]; then
+    ln -s "$MODPATH/webui" "$CAM_MODPATH/webroot"
 fi
 
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
@@ -36,5 +33,3 @@ until [ "$(getprop sys.boot_completed)" = "1" ]; do
 done
 
 [ -f "$MODPATH/action.sh" ] && rm -rf "/data/adb/modules/xiaomiaddon"
-
-# EOF
